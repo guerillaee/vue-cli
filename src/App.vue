d6f459c3e109fabaf725bb7f6876c519b182aabb
DESKTOP-3FNQS12\82640 <ev.bordiuk@gmail.com>
2018-07-31 07:14:00 +0300

    added clean app and main.js files

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,19 @@
 <template>
     <div class="container">
-        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
-        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
         <div class="row">
-            <div class="col-sm-12 text-center">
-                <div class="alert alert-info">
-                    Info: Click on a Quote to delete It
-                </div>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives</h1>
+
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import QuoteGrid from './components/QuoteGrid.vue';
-    import NewQuote from './components/NewQuote.vue';
-    import Header from './components/Header.vue';
-
     export default {
-        data() {
-            return {
-                quotes: [
-                    'To see something'
-                ],
-                maxQuotes: 10
-            }
-        },
-        methods: {
-            newQuote(quote) {
-                if(this.quotes.length >= this.maxQuotes){
-                    return alert('Delete Quotes first')
-                }
-                this.quotes.push(quote);
-            },
-            deleteQuote(index) {
-                this.quotes.splice(index, 1);
-            },
-        },
-        components: {
-            appQuoteGrid: QuoteGrid,
-            appNewQuote: NewQuote,
-            appHeader: Header
-        },
     }
 </script>
 
 <style>
+
 </style>

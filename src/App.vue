3ecc215a5059a63e53b04358ff23a85daee20f8e
DESKTOP-3FNQS12\82640 <ev.bordiuk@gmail.com>
2018-08-01 07:07:42 +0300

    finished directives

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
+                <h1>Built-in Directives</h1>
+                <p v-text="'Some Text'"></p>
+                <p v-html="'<strong>Some strong text</strong>'"></p>
             </div>
         </div>
+        <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
-                <p v-hightlight:background.delayed="'red'">Color this</p>
-                <p v-local-hightlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Local Color this</p>
+                <p v-highlight:background.delayed="'red'">Color this</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color this, too</p>
             </div>
         </div>
     </div>
@@ -18,37 +21,32 @@
 <script>
     export default {
         directives: {
-            'local-hightlight': {
+            'local-highlight': {
                 bind(el, binding, vnode) {
                     var delay = 0;
-                    if(binding.modifiers['delayed']){
+                    if (binding.modifiers['delayed']) {
                         delay = 3000;
                     }
-
-                    if(binding.modifiers['blink']){
+                    if (binding.modifiers['blink']) {
                         let mainColor = binding.value.mainColor;
                         let secondColor = binding.value.secondColor;
                         let currentColor = mainColor;
-
                         setTimeout(() => {
                             setInterval(() => {
-                                currentColor = (currentColor == secondColor) ?
-                                                mainColor : secondColor;
-
-                                if(binding.arg == 'background'){
+                                currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+                                if (binding.arg == 'background') {
                                     el.style.backgroundColor = currentColor;
                                 } else {
                                     el.style.color = currentColor;
                                 }
                             }, binding.value.delay);
                         }, delay);
-
                     } else {
                         setTimeout(() => {
-                            if(binding.arg == 'background'){
+                            if (binding.arg == 'background') {
                                 el.style.backgroundColor = binding.value.mainColor;
                             } else {
-                                el.style.color =  binding.value.mainColor;
+                                el.style.color = binding.value.mainColor;
                             }
                         }, delay);
                     }
@@ -59,5 +57,4 @@
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.directive('hightlight', {
+Vue.directive('highlight', {
     bind(el, binding, vnode) {
         // el.style.backgroundColor = 'green';
         // el.style.backgroundColor = binding.value;
         var delay = 0;
-        if(binding.modifiers['delayed']){
+        if (binding.modifiers['delayed']) {
             delay = 3000;
         }
-
         setTimeout(() => {
-            if(binding.arg == 'background'){
+            if (binding.arg == 'background') {
                 el.style.backgroundColor = binding.value;
             } else {
-                el.style.color =  binding.value;
+                el.style.color = binding.value;
             }
         }, delay);
     }
